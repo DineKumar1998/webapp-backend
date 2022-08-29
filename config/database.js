d35cf5b20cc4c4b07d6d92318d713e98b4ef3dd7
DineKumar1998 <dineshkumar.webhopers@gmail.com>
2022-08-29 11:44:27 +0530

    Changed in db

--- config/database.js
+++ config/database.js
@@ -1,15 +1,29 @@
+const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: 'ec2-44-195-100-240.compute-1.amazonaws.com',
-      port: '5432',
-      database: 'd82ognctalc4k4',
-      username: 'wumfvzfytypskv',
-      password: 'aa6de3cb30839ef7d1b296159130680bf4a23dce4b2ed9bf056066b866bff7d5',
-      ssl: { rejectUnauthorized: false },
-    },
-    debug: false
-  },
-});
+module.exports = ({ env }) => {
+  if (env["NODE_ENV"] === "production") {
+    const config = parse(process.env.HEROKU_POSTGRESQL_AMBER_URL);
+
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  }
+
+  return {}
+};
